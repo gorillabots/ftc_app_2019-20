64cfd951b9bfbfb9cc4c215f79864181c22a18b0
Mikko Neukirch <neukirchm@protonmail.ch>
2019-09-08 14:37:58 -0400

    Created sample opmode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SampleOpMode.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(group="tests", name="SampleOpMode")
+public class SampleOpMode extends LinearOpMode
+{
+    public void runOpMode()
+    {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        waitForStart();
+
+        int n = 1;
+
+        while(opModeIsActive())
+        {
+            telemetry.addData("Status", "Running");
+            telemetry.addData("Count", n++);
+            telemetry.update();
+        }
+
+        telemetry.addData("Status", "Stopped");
+        telemetry.update();
+    }
+}
