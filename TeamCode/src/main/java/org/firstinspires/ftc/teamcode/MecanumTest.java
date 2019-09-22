7bc44c8e8d142f57d5293e44b25540c6ffc02be7
Mikko Neukirch <neukirchm@protonmail.ch>
2019-09-22 13:41:24 -0400

    Created mecanum drive test opmode (untested)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTest.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.components.MecanumDrive;
+
+@TeleOp(group="tests", name="MecanumTest")
+public class MecanumTest extends LinearOpMode
+{
+    MecanumDrive drive;
+
+    @Override
+    public void runOpMode()
+    {
+        drive = new MecanumDrive();
+        drive.init(hardwareMap);
+
+        while(opModeIsActive())
+        {
+            double x = gamepad1.left_stick_x;
+            double y = gamepad1.left_stick_y;
+            double r = gamepad1.right_stick_x;
+
+            drive.go(x, y, r);
+        }
+    }
+}
