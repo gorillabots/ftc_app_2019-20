cb578314dd29039f905e0b493cbceed8e85c29cc
Mikko Neukirch <neukirchm@protonmail.ch>
2020-01-07 19:14:49 -0500

    Implemented getPositionRed and getPositionBlue to CustomVision, modified CustomVisionTest to use it

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/CustomVision.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/CustomVision.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.components;
 
 import android.graphics.Bitmap;
+import android.graphics.Color;
 
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.vuforia.Image;
@@ -66,5 +67,163 @@ public class CustomVision
         frame.close();
 
         return bmp;
+    }
+
+    final int RL_LEFT = 0;
+    final int RL_RIGHT = 150;
+    final int RL_TOP = 100;
+    final int RL_BOTTOM = 175;
+
+    final int RR_LEFT = 185;
+    final int RR_RIGHT = 370;
+    final int RR_TOP = 130;
+    final int RR_BOTTOM = 190;
+
+    final int RL_XINC = 5;
+    final int RL_YINC = 5;
+    final int RL_XYINC = RL_XINC * RL_YINC;
+
+    final int RL_WIDTH = RL_RIGHT - RL_LEFT;
+    final int RL_HEIGHT = RL_BOTTOM - RL_TOP;
+    final int RL_SIZE = RL_WIDTH * RL_HEIGHT;
+    final int RL_SCANSIZE = RL_SIZE / RL_XYINC;
+
+    final int RR_XINC = 5;
+    final int RR_YINC = 5;
+    final int RR_XYINC = RR_XINC * RR_YINC;
+
+    final int RR_WIDTH = RR_RIGHT - RR_LEFT;
+    final int RR_HEIGHT = RR_BOTTOM - RR_TOP;
+    final int RR_SIZE = RR_WIDTH * RR_HEIGHT;
+    final int RR_SCANSIZE = RR_SIZE / RR_XYINC;
+
+    public int getPositionRed()
+    {
+        Bitmap bmp = getImage();
+
+        long suml = 0;
+        long sumr = 0;
+
+        for(int x = RL_LEFT; x < RL_RIGHT; x += RL_XINC)
+        {
+            for (int y = RL_TOP; y < RL_BOTTOM; y += RL_YINC)
+            {
+                int color = bmp.getPixel(x, y);
+
+                suml += Color.red(color) + Color.green(color) + Color.blue(color);
+            }
+        }
+
+        for(int x = RR_LEFT; x < RR_RIGHT; x += RR_XINC)
+        {
+            for (int y = RR_TOP; y < RR_BOTTOM; y += RR_YINC)
+            {
+                int color = bmp.getPixel(x, y);
+
+                sumr += Color.red(color) + Color.green(color) + Color.blue(color);
+            }
+        }
+
+        double avgl = suml / (double) RL_SCANSIZE;
+        double avgr = sumr / (double) RR_SCANSIZE;
+
+        double max = Math.max(avgl, avgr);
+        avgl /= max;
+        avgr /= max;
+
+        if(avgl < 0.65)
+        {
+            tele.addData("Position", "Middle");
+            return 2;
+        }
+        else if(avgr < 0.65)
+        {
+            tele.addData("Position", "Right");
+            return 3;
+        }
+        else
+        {
+            tele.addData("Position", "Left");
+            return 1;
+        }
+    }
+
+    final int BL_LEFT = 0; //TODO: Update numbers
+    final int BL_RIGHT = 180;
+    final int BL_TOP = 135;
+    final int BL_BOTTOM = 205;
+
+    final int BR_LEFT = 210;
+    final int BR_RIGHT = 390;
+    final int BR_TOP = 140;
+    final int BR_BOTTOM = 220;
+
+    final int BL_XINC = 5;
+    final int BL_YINC = 5;
+    final int BL_XYINC = BL_XINC * BL_YINC;
+
+    final int BL_WIDTH = BL_RIGHT - BL_LEFT;
+    final int BL_HEIGHT = BL_BOTTOM - BL_TOP;
+    final int BL_SIZE = BL_WIDTH * BL_HEIGHT;
+    final int BL_SCANSIZE = BL_SIZE / BL_XYINC;
+
+    final int BR_XINC = 5;
+    final int BR_YINC = 5;
+    final int BR_XYINC = BR_XINC * BR_YINC;
+
+    final int BR_WIDTH = BR_RIGHT - BR_LEFT;
+    final int BR_HEIGHT = BR_BOTTOM - BR_TOP;
+    final int BR_SIZE = BR_WIDTH * BR_HEIGHT;
+    final int BR_SCANSIZE = BR_SIZE / BR_XYINC;
+    
+    public int getPositionBlue()
+    {
+        Bitmap bmp = getImage();
+
+        long suml = 0;
+        long sumr = 0;
+
+        for(int x = BL_LEFT; x < BL_RIGHT; x += BL_XINC)
+        {
+            for (int y = BL_TOP; y < BL_BOTTOM; y += BL_YINC)
+            {
+                int color = bmp.getPixel(x, y);
+
+                suml += Color.red(color) + Color.green(color) + Color.blue(color);
+            }
+        }
+
+        for(int x = BR_LEFT; x < BR_RIGHT; x += BR_XINC)
+        {
+            for (int y = BR_TOP; y < BR_BOTTOM; y += BR_YINC)
+            {
+                int color = bmp.getPixel(x, y);
+
+                sumr += Color.red(color) + Color.green(color) + Color.blue(color);
+            }
+        }
+
+        double avgl = suml / (double) BL_SCANSIZE;
+        double avgr = sumr / (double) BR_SCANSIZE;
+
+        double max = Math.max(avgl, avgr);
+        avgl /= max;
+        avgr /= max;
+
+        if(avgl < 0.65)
+        {
+            tele.addData("Position", "Middle");
+            return 2;
+        }
+        else if(avgr < 0.65)
+        {
+            tele.addData("Position", "Right");
+            return 3;
+        }
+        else
+        {
+            tele.addData("Position", "Left");
+            return 1;
+        }
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/CustomVisionTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/CustomVisionTest.java
@@ -31,88 +31,18 @@ public class CustomVisionTest extends LinearOpMode
         long time = System.currentTimeMillis();
         long reftime = time;
 
-        Bitmap bmp = vision.getImage();
-        int width = bmp.getWidth();
-        int height = bmp.getHeight();
+        //Bitmap bmp = vision.getImage();
+        //int width = bmp.getWidth();
+        //int height = bmp.getHeight();
 
         boolean lastA = false;
-
-        final int L_LEFT = 0;
-        final int L_RIGHT = 180;
-        final int L_TOP = 135;
-        final int L_BOTTOM = 205;
-
-        final int R_LEFT = 210;
-        final int R_RIGHT = 390;
-        final int R_TOP = 140;
-        final int R_BOTTOM = 220;
-
-        final int L_XINC = 5;
-        final int L_YINC = 5;
-        final int L_XYINC = L_XINC * L_YINC;
-
-        final int L_WIDTH = L_RIGHT - L_LEFT;
-        final int L_HEIGHT = L_BOTTOM - L_TOP;
-        final int L_SIZE = L_WIDTH * L_HEIGHT;
-        final int L_SCANSIZE = L_SIZE / L_XYINC;
-
-        final int R_XINC = 5;
-        final int R_YINC = 5;
-        final int R_XYINC = R_XINC * R_YINC;
-
-        final int R_WIDTH = R_RIGHT - R_LEFT;
-        final int R_HEIGHT = R_BOTTOM - R_TOP;
-        final int R_SIZE = R_WIDTH * R_HEIGHT;
-        final int R_SCANSIZE = R_SIZE / R_XYINC;
 
         while(opModeIsActive())
         {
             i++;
-            bmp = vision.getImage();
+            //bmp = vision.getImage();
 
-            if(gamepad1.a && !lastA)
-            {
-                File path = Environment.getExternalStorageDirectory();
-                String file = "pics/" + i + ".png";
-                File dest = new File(path, file);
-
-                try (FileOutputStream out = new FileOutputStream(dest))
-                {
-                    bmp.compress(Bitmap.CompressFormat.PNG, 100, out); // bmp is your Bitmap instance
-                    // PNG is a lossless format, the compression factor (100) is ignored
-                }
-                catch (IOException e)
-                {
-                    e.printStackTrace();
-                }
-            }
-            lastA = gamepad1.a;
-
-            long suml = 0;
-            long sumr = 0;
-
-            for(int x = L_LEFT; x < L_RIGHT; x += L_XINC)
-            {
-                for (int y = L_TOP; y < L_BOTTOM; y += L_YINC)
-                {
-                    int color = bmp.getPixel(x, y);
-
-                    suml += Color.red(color) + Color.green(color) + Color.blue(color);
-                }
-            }
-
-            for(int x = R_LEFT; x < R_RIGHT; x += R_XINC)
-            {
-                for (int y = R_TOP; y < R_BOTTOM; y += R_YINC)
-                {
-                    int color = bmp.getPixel(x, y);
-
-                    sumr += Color.red(color) + Color.green(color) + Color.blue(color);
-                }
-            }
-
-            double avgl = suml / (double) L_SCANSIZE;
-            double avgr = sumr / (double) R_SCANSIZE;
+            int pos = vision.getPositionRed();
 
             time = System.currentTimeMillis();
 
@@ -125,32 +55,9 @@ public class CustomVisionTest extends LinearOpMode
 
             telemetry.addData("i", i);
             telemetry.addData("fps", fps);
-            telemetry.addData("width", width);
-            telemetry.addData("height", height);
-            telemetry.addData("suml", suml);
-            telemetry.addData("avgl", avgl);
-            telemetry.addData("sumr", sumr);
-            telemetry.addData("avgr", avgr);
-
-            double max = Math.max(avgl, avgr);
-            avgl /= max;
-            avgr /= max;
-
-            telemetry.addData("avglr", avgl);
-            telemetry.addData("avgrr", avgr);
-
-            if(avgl < 0.65)
-            {
-                telemetry.addData("Position", "Middle");
-            }
-            else if(avgr < 0.65)
-            {
-                telemetry.addData("Position", "Right");
-            }
-            else
-            {
-                telemetry.addData("Position", "Left");
-            }
+            //telemetry.addData("width", width);
+            //telemetry.addData("height", height);
+            telemetry.addData("Position int", pos);
 
             telemetry.update();
         }
